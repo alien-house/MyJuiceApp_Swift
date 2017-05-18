d3476ee68cf5ad3bd6617d8ce4b4dfe39376fef2
alien-house <info@alien-house.com>
2017-05-17 17:21:31 -0700

    add credit

---
+++ MyJuice/MyJuice_Swift/MyJuice_Swift/MyJuice_Swift/TabBarNaviViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TabBarNaviViewController.swift
+//  MyJuice_Swift
+//
+//  Created by sin on 2017/05/13.
+//  Copyright © 2017年 shinji. All rights reserved.
+//
+
+import UIKit
+
+class TabBarNaviViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
